376129cab9fb7b4f8b6eb68a974e2d732ff20522
endsound <endsound0211@gmail.com>
2018-05-22 17:22:44 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {BsTableModule} from "./bs-table/bs-table.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BsTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
