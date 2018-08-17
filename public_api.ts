61cc9818dd115a092ace4b943014fb791d15f6b8
endsound <endsound0211@gmail.com>
2018-08-17 11:09:52 +0800

    export all component, pipe, token

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,2 @@
-export * from './src/app/bs-table/bs-table.module'
+// export * from './src/app/bs-table/bs-table.module'
+export * from './src/app/bs-table/index'
--- src/app/bs-table/index.ts
+++ src/app/bs-table/index.ts
@@ -1,6 +1,17 @@
-export {BsTableModule} from "./bs-table.module";
-export {NgTableComponent} from "./ng-table/ng-table.component";
-export {NgTableColComponent} from "./ng-table-col/ng-table-col.component";
-export {NgSdTableComponent} from "./ng-sd-table/ng-sd-table.component";
-export {NgTableToolBarComponent} from "./ng-table-tool-bar/ng-table-tool-bar.component";
-export {NgTableGeneralSearchComponent} from "./ng-table-general-search/ng-table-general-search.component";
+export * from "./bs-table.module";
+export * from "./page";
+export * from "./template-type.pipe";
+
+export * from "./ng-table-token";
+export * from "./ng-table/ng-table.component";
+export * from "./ng-table-col/ng-table-col.component";
+export * from "./ng-sd-table/ng-sd-table.component";
+export * from "./ng-table-tool-bar/ng-table-tool-bar.component";
+export * from "./ng-table-general-search/ng-table-general-search.component";
+export * from "./ng-table-advanced-search/ng-table-advanced-search.component";
+export * from "./ng-table-hide-col/ng-table-hide-col.component";
+
+export * from "./locale/ng-table-i18n.service";
+export * from "./locale/ng-table-en-us.service";
+export * from "./locale/ng-table-zh-tw.service";
+
